0e0de0d8db01c41ffac8a63fee069f7e6e743238
valajan <s.anth@outlook.fr>
2020-01-27 17:22:58 +0100

    Add unawaited on main.dart

--- main.dart
+++ main.dart
@@ -1,17 +1,18 @@
 // Utilisation basique du routeur
 // Import du router
 import 'lib/src/autoloader.dart';
+import 'package:pedantic/pedantic.dart';
 // Déclaration de la fonction main non async
 void main() async {
   await Prebuild().main();
   // Appel de l'objet Router avec une configuration minimale
   // On peut utiliser des options myView, errorPage et myPort
-  Router()
+  unawaited(Router()
   // Démarrage du router qui s'occupe de créer un serveur web
   // D'écouter les requêtes et de retourner un résultat en
   // Fonction du fichier de configuration qui est indispensable
   // Paramètre logger optionnel
-    .router(logger: false);
+    .router(logger: false));
   // Configuration du router avec le fichier de configuration
   // renseigné plus haut
     // ..config(routeConfig);
