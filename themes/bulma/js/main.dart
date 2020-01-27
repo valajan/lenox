258ba907b9c7c0e95ffe10a334f6e52529be8028
valajan <s.anth@outlook.fr>
2020-01-27 21:23:50 +0100

    New query selector on bulma theme

--- themes/bulma/js/main.dart
+++ themes/bulma/js/main.dart
@@ -1,7 +1,24 @@
+// Bulma CSS designer
 import 'dart:html';
 void main() {
+  // h1
   document.querySelector('h1').setAttribute('class', 'title is-1');
+  // h2
   document.querySelector('h2').setAttribute('class', 'subtitle is-2');
+  // h3
   document.querySelector('h3').setAttribute('class', 'subtitle is-3');
+  // button
   document.querySelector('button').setAttribute('class', 'button is-link is-light');
+  // section
+  document.querySelector('section').setAttribute('class', 'section');
+  // input
+  document.querySelector('section').setAttribute('class', 'input');
+  // textarea
+  document.querySelector('textarea').setAttribute('class', 'textarea');
+  // checkbox
+  document.querySelector('checkbox').setAttribute('class', 'checkbox');
+  // tag
+  document.querySelector('span').setAttribute('class', 'tag');
+  // table
+  document.querySelector('table').setAttribute('class', 'table');
 }
