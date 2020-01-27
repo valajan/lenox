bc63e6f62dbc7078571c1c669b8a2284a88e7f19
valajan <s.anth@outlook.fr>
2020-01-27 17:46:12 +0100

    Delete theme config file

--- lib/src/autoloader.dart
+++ lib/src/autoloader.dart
@@ -3,6 +3,5 @@ export '../router.dart';
 export 'config.dart';
 export 'build_request.dart';
 export 'content.dart';
-export 'theme_config.dart';
 export 'static_directory.dart';
 export 'prebuild.dart';
