033a15d45cc414a28b7f401ceefd632faffdd439
valajan <s.anth@outlook.fr>
2020-02-04 21:14:30 +0100

    Style change

--- main.dart
+++ main.dart
@@ -7,13 +7,12 @@ Future<void> main() async {
   await Prebuild().main();
   // Appel de l'objet Router avec une configuration minimale
   // On peut utiliser des options myView, errorPage et myPort
-  Future.delayed(Duration(seconds: 1), () => 
-  Router()
+  await Router()
   // Démarrage du router qui s'occupe de créer un serveur web
   // D'écouter les requêtes et de retourner un résultat en
   // Fonction du fichier de configuration qui est indispensable
   // Paramètre logger optionnel
-    .router(logger: false));
+    .router(logger: false);
   // Configuration du router avec le fichier de configuration
   // renseigné plus haut
     // ..config(routeConfig);
