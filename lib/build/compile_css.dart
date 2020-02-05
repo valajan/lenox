b8096dae017cf362566fff20c0754426efadda81
valajan <s.anth@outlook.fr>
2020-02-05 19:56:32 +0100

    Check if css folder exist in build process

--- lib/build/compile_css.dart
+++ lib/build/compile_css.dart
@@ -9,11 +9,15 @@ class CompileCss {
     config.setConfig('config.yaml');
     await config.getter();
     theme = config.getTheme();
-    Directory('themes/$theme/css/').list().listen((FileSystemEntity contents) {
-      print(contents);
-      cssSafe = contents.path.replaceAll('themes/$theme/css/', '');
-      File(contents.path).copy('build/css/$cssSafe');
-    });
+    if (Directory('themes/$theme/css/').existsSync()) {
+      Directory('themes/$theme/css/')
+          .list()
+          .listen((FileSystemEntity contents) {
+        print(contents);
+        cssSafe = contents.path.replaceAll('themes/$theme/css/', '');
+        File(contents.path).copy('build/css/$cssSafe');
+      });
+    }
     print('Build done ! Check the build folder');
   }
 }
