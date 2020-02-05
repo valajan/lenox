00cbad8533effc98bf1b52f808fba74d4a705ac2
valajan <s.anth@outlook.fr>
2020-02-05 19:52:32 +0100

    Check if assets folder exist in build process

--- lib/build/compile_assets.dart
+++ lib/build/compile_assets.dart
@@ -9,9 +9,14 @@ class CompileAssets {
     config.setConfig('config.yaml');
     await config.getter();
     theme = config.getTheme();
-    Directory('themes/$theme/assets/').list().listen((FileSystemEntity contents) {
-      assetSafe = contents.path.replaceAll('themes/$theme/assets/', '');
-      File(contents.path).copy('build/assets/$assetSafe');
-    });
+    if (Directory('themes/$theme/assets/').existsSync()) {
+      Directory('themes/$theme/assets/')
+          .list()
+          .listen((FileSystemEntity contents) {
+        print(contents);
+        assetSafe = contents.path.replaceAll('themes/$theme/assets/', '');
+        File(contents.path).copy('build/assets/$assetSafe');
+      });
+    }
   }
 }
