3d4355c69ebf3c94c57261d2b41e0fc0026e471d
valajan <s.anth@outlook.fr>
2020-12-03 23:32:31 +0100

    Remove option from init command

--- lib/src/init.dart
+++ lib/src/init.dart
@@ -3,6 +3,6 @@ import 'package:process_run/process_run.dart';
 class Init {
   void main() async {
     print('Cloning the repo...');
-    await run('git clone https://github.com/valajan/lenox_content.git', [''], verbose: true);
+    await run('git clone https://github.com/valajan/lenox_content.git', [], verbose: true);
   }
 }
